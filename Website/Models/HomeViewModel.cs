27fd3b027b284814336c05583bdfcaa451f0415f
Kyle Pelham <kylepelham9000@gmail.com>
2024-09-08 01:03:24 +0100

    task 6: added upcoming events without attendee widget

--- Website/Models/HomeViewModel.cs
+++ Website/Models/HomeViewModel.cs
@@ -3,5 +3,6 @@
 public class HomeViewModel
 {
     public required IEnumerable<Event> UpcomingEvents { get; init; }
+    public required IEnumerable<Event> UpcomingEventsWithoutAttendees { get; init; }
     public required IReadOnlyCollection<(Employee, long)> EmployeesWithEventCount { get; init; }
 }
